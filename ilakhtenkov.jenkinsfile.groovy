ee9139b9bc61b4a82e185aecebff405c9e6796ce
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-23 15:51:01 +0300

    BUILD stage fixing

--- ilakhtenkov.jenkinsfile.groovy
+++ ilakhtenkov.jenkinsfile.groovy
@@ -1,17 +1,1 @@
-node {
-    def repositoryUrl = "https://github.com/MNT-Lab/mntlab-pipeline.git"
-    def branch = "ilakhtenkov"
-
-    stage('PREPARATION') {
-        git branch: branch, url: repositoryUrl
-    }
-    stage('BUILD') {
-        rtGradle.tool = "gradle3.3"
-        //rtGradle.deployer repo:'ext-release-local', server: server
-        //rtGradle.resolver repo:'remote-repos', server: server
-        buildInfo = rtGradle.run rootDir: "/", buildFile: 'build.gradle', tasks: 'clean build'
-    }
-
-    echo "Hello world"
-}
-
+task11/src/ilakhtenkov.jenkinsfile.groovy

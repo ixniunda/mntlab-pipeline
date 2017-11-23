0d95ba8f2b8b58e16411bcb0c9122f497a7e326e
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-23 15:12:10 +0300

    Preparation, build stages initiated

--- ilakhtenkov.jenkinsfile.groovy
+++ ilakhtenkov.jenkinsfile.groovy
@@ -1,3 +1,17 @@
 node {
+    def repositoryUrl = "https://github.com/MNT-Lab/mntlab-pipeline.git"
+    def branch = "ilakhtenkov"
+
+    stage('Clone sources') {
+        git branch: branch, url: repositoryUrl
+    }
+    stage('Artifactory configuration') {
+        rtGradle.tool = "gradle3.3"
+        //rtGradle.deployer repo:'ext-release-local', server: server
+        //rtGradle.resolver repo:'remote-repos', server: server
+        buildInfo = rtGradle.run rootDir: "/", buildFile: 'build.gradle', tasks: 'clean build'
+    }
+
     echo "Hello world"
 }
+

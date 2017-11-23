525ac684b0ee3dcee803aac55fdd376b06edfc27
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-23 16:06:47 +0300

    BUILD stage fixing

--- ilakhtenkov.jenkinsfile.groovy
+++ ilakhtenkov.jenkinsfile.groovy
@@ -1,16 +1,28 @@
-node {
-    def repositoryUrl = "https://github.com/MNT-Lab/mntlab-pipeline.git"
-    def branch = "ilakhtenkov"
+import hudson.model.Run
 
-    stage('PREPARATION') {
-        git branch: branch, url: repositoryUrl
+def String exceptionMessage
+try {
+    node {
+        def repositoryUrl = "https://github.com/MNT-Lab/mntlab-pipeline.git"
+        def branch = "ilakhtenkov"
+
+        stage('PREPARATION') {
+            exceptionMessage = "PREPARARION FAILED"
+            git branch: branch, url: repositoryUrl
+        }
+        stage('BUILD') {
+            exceptionMessage = "BUILD FAILED"
+            def rtGradle = Artifactory.newGradleBuild()
+            rtGradle.tool = "gradle3.3"
+            rtGradle.deployer repo:'ext-release-local', server: server
+            rtGradle.resolver repo:'remote-repos', server: server
+            buildInfo = rtGradle.run rootDir: "/", buildFile: 'build.gradle', tasks: 'clean build'
+        }
+        echo "Hello world"
     }
-    stage('BUILD') {
-        def rtGradle = Artifactory.newGradleBuild()
-        rtGradle.tool = "gradle3.3"
-        rtGradle.deployer repo:'ext-release-local', server: server
-        rtGradle.resolver repo:'remote-repos', server: server
-        buildInfo = rtGradle.run rootDir: "/", buildFile: 'build.gradle', tasks: 'clean build'
-    }
-    echo "Hello world"
+} catch(Exception ex) {
+    println exceptionMessage
 }
+
+println("Let's move on after the exception");
+

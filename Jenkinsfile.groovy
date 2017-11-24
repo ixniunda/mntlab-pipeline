9fecd6d345cc1a70dd1fb220e6a2faa893f00b26
Ihar Vauchok <napa93@mail.ru>
2017-11-24 13:09:18 +0000

    Fix/Preparation stage in Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,4 +5,4 @@ node {
     stage('Preparation (Checking out)') {
         checkout scm: [$class: 'GitSCM', branches: [[name: '*/ivauchok']], userRemoteConfigs: [[url: 'https://github.com/MNT-Lab/mntlab-pipeline.git']]]
     }
-
+}

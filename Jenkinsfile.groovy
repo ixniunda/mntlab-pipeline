87111f869cbae9f8a76eec1a1a590d552b252e81
Ihar Vauchok <napa93@mail.ru>
2017-11-24 13:30:15 +0000

    Feature/Added Building stage to Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,4 +5,11 @@ node {
     stage('Preparation (Checking out)') {
         checkout scm: [$class: 'GitSCM', branches: [[name: '*/ivauchok']], userRemoteConfigs: [[url: 'https://github.com/MNT-Lab/mntlab-pipeline.git']]]
     }
+    stage('Building code') {
+        steps {
+            withGradle(gradle : 'gradle3.3') {
+                sh 'gradle clean build'
+            }
+        }
+    }
 }

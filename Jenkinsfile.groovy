9dfca89c153c230441df8f6215c251fbad0e1ca5
Ihar Vauchok <napa93@mail.ru>
2017-11-24 13:35:44 +0000

    Fix_2/Building stage in Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,8 +6,6 @@ node {
         checkout scm: [$class: 'GitSCM', branches: [[name: '*/ivauchok']], userRemoteConfigs: [[url: 'https://github.com/MNT-Lab/mntlab-pipeline.git']]]
     }
     stage('Building code') {
-            withGradle(gradle : 'gradle3.3') {
                 sh 'gradle clean build'
-            }
     }
 }

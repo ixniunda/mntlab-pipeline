b4ada7558f44cba1a62745f3bece40e3b012652e
Lakhtenkov-iv <lakhtenkov@gmail.com>
2017-11-27 13:43:16 +0300

    General fixing

--- ilakhtenkov.jenkinsfile.groovy
+++ ilakhtenkov.jenkinsfile.groovy
@@ -1,6 +1,11 @@
 node ("EPBYMINW2033") {
     def repositoryUrl = "https://github.com/MNT-Lab/mntlab-pipeline.git"
     def branch = "ilakhtenkov"
+    def groupId = "com.epam.mntlab.pipeline"
+    def artifactId = "gradle-simple"
+    def version = "${env.BUILD_NUMBER}"
+    def nexusServer = "http://10.6.205.59:8081"
+    def repository = "Artifact_storage"
     def GRADLE_HOME = tool name: 'gradle3.3', type: 'hudson.plugins.gradle.GradleInstallation'
 
     stage('PREPARATION') {
@@ -10,7 +15,7 @@ node ("EPBYMINW2033") {
         }
         catch (Exception error){
             println ("PREPARATION Failed")
-            postToSlack ("${env.BUILD_TAG} PREPARATION Failed", "#general", "bot.ilakhtenkov")
+            postToSlack ("${env.BUILD_TAG} PREPARATION Failed")
             throw error
         }
     }
@@ -20,7 +25,7 @@ node ("EPBYMINW2033") {
         }
         catch (Exception error){
             println("BUILD Failed")
-            postToSlack ("${env.BUILD_TAG} BUILD Failed. See details:${env.BUILD_URL}", "#general", "bot.ilakhtenkov")
+            postToSlack ("${env.BUILD_TAG} BUILD Failed. See details:${env.BUILD_URL}")
             throw error
         }
     }
@@ -36,7 +41,7 @@ node ("EPBYMINW2033") {
         }
         catch (Exception error){
             println("TEST Failed")
-            postToSlack ("${env.BUILD_TAG} TEST Failed", "#general", "bot.ilakhtenkov")
+            postToSlack ("${env.BUILD_TAG} TEST Failed")
             throw error
         }
     }
@@ -47,21 +52,26 @@ node ("EPBYMINW2033") {
         }
         catch (Exception error){
             println("TRIGGER-CHILD Failed")
-            postToSlack ("${env.BUILD_TAG} TRIGGER-CHILD Failed", "#general", "bot.ilakhtenkov")
+            postToSlack ("${env.BUILD_TAG} TRIGGER-CHILD Failed")
             throw error
         }
     }
     stage('PUBLISHING-RESULTS') {
         try {
             sh "tar -xzf  ${branch}_dsl_script.tar.gz"
-            sh "tar -czf  pipeline-${branch}-${env.BUILD_NUMBER}.tar.gz ./dsl/dsl_script.groovy ./ilakhtenkov.jenkinsfile.groovy ./build/libs/gradle-simple.jar"
-            archiveArtifacts 'pipeline-ilakhtenkov-*.tar.gz'
-            sh "curl -v --user 'jenkins:jenkins' --upload-file './pipeline-${branch}-${env.BUILD_NUMBER}.tar.gz' 'http://10.6.205.59:8081/repository/Artifact_storage/com/epam/mntlab/pipeline/gradle-simple-${env.BUILD_NUMBER}/${env.BUILD_NUMBER}/gradle-simple-${env.BUILD_NUMBER}-${env.BUILD_NUMBER}.tar.gz'"
+            sh "tar -czf  pipeline-${branch}-${version}.tar.gz ./dsl/dsl_script.groovy ./ilakhtenkov.jenkinsfile.groovy ./build/libs/gradle-simple.jar"
+            archiveArtifacts "pipeline-${branch}-*.tar.gz"
+            def finder = groupId =~ /\w+/
+            def grouppath = []
+            for (i = 0; i < finder.size(); i++) {
+                grouppath += "${finder[i]}"
+            }
+            sh "curl -v --user 'jenkins:jenkins' --upload-file './pipeline-${branch}-${env.BUILD_NUMBER}.tar.gz' '${nexusServer}/repository/${repository}/${grouppath.join('/')}/${artifactId}-${version}/${version}/${artifactId}-${version}-${version}.tar.gz'"
             //sh "curl -v --user 'jenkins:jenkins' --upload-file './pipeline-${branch}-${env.BUILD_NUMBER}.tar.gz' 'http://nexus.local/repository/Artifact_storage/com/epam/mntlab/pipeline/gradle-simple-${env.BUILD_NUMBER}/${env.BUILD_NUMBER}/gradle-simple-${env.BUILD_NUMBER}-${env.BUILD_NUMBER}.tar.gz'"
         }
         catch (Exception error){
             println("PUBLISHING-RESULTS Failed")
-            postToSlack ("${env.BUILD_TAG} PUBLISHING-RESULTS Failed", "#general", "bot.ilakhtenkov")
+            postToSlack ("${env.BUILD_TAG} PUBLISHING-RESULTS Failed")
             throw error
         }
     }
@@ -71,27 +81,29 @@ node ("EPBYMINW2033") {
         }
         catch (Exception error){
             println("APPROVAL Failed")
-            postToSlack ("${env.BUILD_TAG} APPROVAL Failed", "#general", "bot.ilakhtenkov")
+            postToSlack ("${env.BUILD_TAG} APPROVAL Failed")
             throw error
         }
     }
     stage('DEPLOYING') {
         try {
-            sh "java -jar build/libs/gradle-simple.jar"
+            sh "java -jar build/libs/${artifactId}.jar"
             }
         catch (Exception error){
             println("DEPLOYING Failed")
-            postToSlack ("${env.BUILD_TAG} DEPLOYING failed", "#general", "bot.ilakhtenkov")
+            postToSlack ("${env.BUILD_TAG} DEPLOYING failed")
             throw error
         }
     }
     stage('STATUS') {
         println "SUCCESS"
-        postToSlack ("${env.BUILD_TAG} Successfully deployed. See details:${env.BUILD_URL} ", "#general", "bot.ilakhtenkov")
+        postToSlack ("${env.BUILD_TAG} Successfully deployed. See details:${env.BUILD_URL} ")
     }
 }
 
-def postToSlack (String message, String channel, String userName) {
+def postToSlack (String message) {
     def webhookUrl = "https://hooks.slack.com/services/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"
+    def channel = "#general"
+    def userName = "bot.ilakhtenkov"
     sh "curl -X POST --data-urlencode \'payload={\"channel\": \"${channel}\", \"username\": \"${userName}\", \"text\": \"${message}\", \"icon_emoji\": \":chicken:\"}\' \"${webhookUrl}\""
 }
